35db3158d704a78e369c77835caf87c0b092fed2
frako23 <frako23@gmail.com>
2025-05-13 16:15:52 -0400

    feat: Update async-storage dependency to use caret versioning and add Firebase configuration

---
+++ config/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCcB4o19tsCMBHvbJ0woC0w28ORjzbILz8",
+    authDomain: "expense-tracker-3ff12.firebaseapp.com",
+    projectId: "expense-tracker-3ff12",
+    storageBucket: "expense-tracker-3ff12.firebasestorage.app",
+    messagingSenderId: "169732008737",
+    appId: "1:169732008737:web:6ba2b64d8a908c8d6aac60"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
